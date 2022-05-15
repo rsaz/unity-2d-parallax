6dff5f1579b87ec8fee03cadb9738c9da74282fc
Richard Zampieri <rc_scott48@hotmail.com>
2022-05-15 03:14:42 -0700

    Added 2D Parallax Project

---
+++ Assets/scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
---
+++ Assets/scripts/ParallaxEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    public Vector2 parallaxMultiplier;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private float textureUnitSizeX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+
+        // fazer com que o background acompanhe a camera
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // quanto a camera se moveu ate o frame atual
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxMultiplier.x, deltaMovement.y * parallaxMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+        // layer seguir a camera
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }
+    }
+}
